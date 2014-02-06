c215522e2da052ef6939fc906bea57e6f9a36b65
lshain <lshain.gyh@gmail.com>
2014-02-06 21:32:51 +0800

    add fun git_remote_set_url

---
+++ git_remote_set_url.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROJECT_PATH=""
+PROJECT_GIT_PATH=""
+CUR_PATH=$PWD
+
+cat $CUR_PATH/project | while read p
+do
+    if [ "$PROJECT_PATH" = "" ]
+    then
+         PROJECT_PATH=$p
+         continue
+    fi
+
+    if [ "$PROJECT_GIT_PATH" = "" ]
+    then
+         PROJECT_GIT_PATH=$p
+    fi
+
+    if [ "$PROJECT_PATH" != "" ]
+    then 
+         if [ "$PROJECT_GIT_PATH" != "" ]
+         then
+              cd $CUR_PATH/../$PROJECT_PATH
+              git remote set-url origin git@github.com:"$PROJECT_GIT_PATH".git
+              PROJECT_PATH=""
+              PROJECT_GIT_PATH=""
+         fi
+    fi
+done
+
